5b4fcca2506cc4d5324acf929c2b2a13f06fddf7
Conejitobrr <110344521+Conejitobrr@users.noreply.github.com>
2024-02-04 22:16:54 -0500

    Create Prueba audios.js

---
+++ plugins/Prueba audios.js
@@ -0,0 +1,24 @@
+//CREDITOS PARA >> https://github.com/BrunoSobrino
+
+let handler = m => m
+handler.all = async function (m) {
+let chat = global.db.data.chats[m.chat]
+if (chat.isBanned) return
+global.db.data.users[m.sender].money += 50
+global.db.data.users[m.sender].exp += 50  
+    
+if (/^hola$/i.test(m.text) && chat.audios) {  
+if (!db.data.chats[m.chat].audios && m.isGroup) throw 0    
+let vn = './media/Hola.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendMessage(m.chat, { audio: { url: vn }, fileName: 'error.mp3', mimetype: 'audio/mp4', ptt: true }, { quoted: m })}
+
+if (!chat.isBanned && m.text.match(/(Jejeje)/gi)) {
+if (!db.data.chats[m.chat].audios) return;
+const vn = './media/Jejeje.mp3';
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendMessage(m.chat, { audio: { url: vn }, fileName: 'error.mp3', mimetype: 'audio/mp4', ptt: true }, { quoted: m })} 
+
+
+return !0 }
+export default handler
