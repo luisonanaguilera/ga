45199e5d924514dc38e3b0158f1cc1f7e48542dc
Conejitobrr <110344521+Conejitobrr@users.noreply.github.com>
2024-02-04 20:33:35 -0500

    Update Aprueba.js

--- plugins/Aprueba.js
+++ plugins/Aprueba.js
@@ -1,70 +1,28 @@
-import fetch from 'node-fetch';
-import translate from '@vitalets/google-translate-api';
-const handler = (m) => m;
-
-handler.before = async (m) => {
-let Prefijo = false;
-const prefixRegex = global.prefix;
-if (prefixRegex.test(m.text) && !opts['gconly']) Prefijo = true;
-const bot = global.db.data.settings[conn.user.jid]   
-if (bot.modoia && !m.isGroup && !Prefijo && !m.fromMe && m.text !== '') {
-if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return;
-let textodem = m.text;
-const name = conn.getName(m.sender)
-const namedem = `${name || 'Sin Definir'}`
-const sytm = await fetch(`https://raw.githubusercontent.com/GataNina-Li/GataBot-MD/master/src/chatgpt_indicciones.txt`).then(v => v.text()); 
-const sistema1 = sytm.replace('@name', namedem)
+import fetch from 'node-fetch'
+import axios from 'axios'
+import translate from '@vitalets/google-translate-api'
+import { Configuration, OpenAIApi } from 'openai'
+const configuration = new Configuration({ organization: global.openai_org_id, apiKey: global.openai_key });
+const openaiii = new OpenAIApi(configuration);
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (usedPrefix == 'a' || usedPrefix == 'A') return    
+if (!text) throw `*${lenguajeGB['smsAvisoMG']()}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉𝘼 𝙋𝙀𝙏𝙄𝘾𝙄𝙊𝙉 𝙊 𝙐𝙉𝘼 𝙊𝙍𝘿𝙀𝙉 𝙋𝘼𝙍𝘼 𝙐𝙎𝘼𝙍 𝙇𝘼 𝙁𝙐𝙉𝘾𝙄𝙊𝙉 𝘿𝙀𝙇 𝘾𝙃𝘼𝙏𝙂𝙋𝙏\n\n❏ 𝙀𝙅𝙀𝙈𝙋𝙇𝙊 𝘿𝙀 𝙋𝙀𝙏𝙄𝘾𝙄𝙊𝙉𝙀𝙎 𝙔 𝙊𝙍𝘿𝙀𝙉𝙀𝙎\n❏ ${usedPrefix + command} Recomienda un top 10 de películas de acción\n❏ ${usedPrefix + command} Codigo en JS para un juego de cartas`     
 try {
-await conn.sendPresenceUpdate('composing', m.chat)
-async function getOpenAIChatCompletion(texto) {
-const openaiAPIKey = global.openai_key;
-let chgptdb = global.chatgpt.data.users[m.sender];
-chgptdb.push({ role: 'user', content: texto });
-const url = "https://api.openai.com/v1/chat/completions";
-const headers = { "Content-Type": "application/json", "Authorization": `Bearer ${openaiAPIKey}` };
-const data = { "model": "gpt-3.5-turbo", "messages": [{ "role": "system", "content": sistema1 }, ...chgptdb, ]};
-const response = await fetch(url, {method: "POST", headers: headers, body: JSON.stringify(data)});
-const result = await response.json();
-const finalResponse = result.choices[0].message.content;
-return finalResponse;
-};
-let respuesta = await getOpenAIChatCompletion(textodem);
-m.reply(`${respuesta}`.trim());
-return;
-} catch {    
-try {
-const fgapi1 = await fetch(`https://api-fgmods.ddns.net/api/info/openai?text=${textodem}&symsg=${sistema1}&apikey=XlwAnX8d`);
-const fgjson1 = await fgapi1.json();
-if (fgjson1.result == 'error' || fgjson1.result == '' || !fgjson1.result) return XD;
-m.reply(`${fgjson1.result}`.trim());
-return;    
+conn.sendPresenceUpdate('composing', m.chat)  
+let syms = `Actuaras como un Bot de WhatsApp el cual fue creado por GataNina-Li, tu seras GataBot-MD`
+let res = await gpt.ChatGpt(text, syms)
+await m.reply(res.text)
 } catch {
-try {
-const vihangayt1 = await fetch(`https://vihangayt.me/tools/chatgpt?q=${textodem}`);
-const vihangaytjson1 = await vihangayt1.json();
-if (vihangaytjson1.data == 'error' || vihangaytjson1.data == '' || !vihangaytjson1.data) return XD;
-m.reply(`${vihangaytjson1.data}`.trim());
-return;
-} catch {
-try {
-const vihangayt2 = await fetch(`https://vihangayt.me/tools/chatgpt2?q=${textodem}`);
-const vihangaytjson2 = await vihangayt2.json();
-if (vihangaytjson2.data == 'error' || vihangaytjson2.data == '' || !vihangaytjson2.data) return XD; 
-m.reply(`${vihangaytjson2.data}`.trim());
-return;    
-} catch {
+try {   
+let ia2 = await fetch(`https://api.amosayomide05.cf/gpt/?question=${text}&string_id=${m.sender}`) //fetch(`https://api.ibeng.tech/api/info/openai?text=${text}&apikey=tamvan`)
+let resu2 = await ia2.json()
+m.reply(resu2.response.trim())    
+} catch {        
 try {    
-const vihangayt3 = await fetch(`https://vihangayt.me/tools/chatgpt3?q=${textodem}`);
-const vihangaytjson3 = await vihangayt3.json();
-if (vihangaytjson3.data == 'error' || vihangaytjson3.data == '' || !vihangaytjson3.data) return XD;
-m.reply(JSON.parse(`${vihangaytjson3.data}`).trim()); 
+let tioress = await fetch(`https://api.lolhuman.xyz/api/openai-turbo?apikey=${lolkeysapi}&text=${text}`)
+let hasill = await tioress.json()
+m.reply(`${hasill.result}`.trim())   
 } catch {    
-const akuariapi2 = await fetch(`https://api.akuari.my.id/ai/gpt?chat=${textodem}`);
-const akuariapijson2 = await akuariapi2.json();
-const akuariapiresult2 = await translate(`${akuariapijson2.respon}`, {to: 'es', autoCorrect: true});
-m.reply(`${akuariapiresult2.text}`.trim());  
-return;    
-}}}}}}
-return true;
-};
-export default handler;
+}}}}
+handler.command = ['openai', 'chatgpt', 'ia', 'robot']
+export default handler
